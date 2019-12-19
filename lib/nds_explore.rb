627a463b3e430b9794bece5584698cd7e259901d
Bruno Silva <brunosilvinosilva@gmail.com>
2019-12-19 17:18:51 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -15,7 +15,7 @@ def print_first_directors_movie_titles
    array_index = 0 
   while array_index < directors_database[0][:movies].count do 
    print directors_database[0][:movies][0]
-    array_index += 1 
+    puts directors_database[0][:movies][array_index]
   end 
 end 
 
