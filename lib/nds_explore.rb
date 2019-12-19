41627f591eae9bf1bfec941b162fd0393b1125f1
Bruno Silva <brunosilvinosilva@gmail.com>
2019-12-19 17:09:55 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -15,8 +15,8 @@ def print_first_directors_movie_titles
    spielberg_films = [] 
    array_index = 0 
   while array_index < directors_database[0][:movies].count do 
-    if directors_database[0][:movies] == :title 
-    spielberg_films << directors_database[0][:movies] 
+    directors_database[0][:movies]
+    
   end 
   array_index += 1 
 end 
