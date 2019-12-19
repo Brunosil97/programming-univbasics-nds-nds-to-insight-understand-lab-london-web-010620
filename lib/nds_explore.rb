3f2a4bfe628bc21a6e3d9569099facdcd4be9677
Bruno Silva <brunosilvinosilva@gmail.com>
2019-12-19 16:37:00 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,18 +10,9 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_title
+  directors_database[0]
+  directors_database
   
-  
-   row_index = 0 
-   while row_index < directors_database.count do 
-     element_index = 0 
-     while element_index < directors_database[row_index].count do 
-       puts directors_database[element_index][row_index]
-       element_index += 1 
-     end 
-     row_index +=1 
-   end 
-    directors_database
 end    
 
 
